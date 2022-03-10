0a93da840d35902adfa72961b1c5c2516758cc8a
hiromiakitao@gmail.com <hiromiakitao@gmail.com>
2022-03-09 19:14:30 -0600

    repaso

---
+++ 2_repaso_conceptos/js/app.js
@@ -0,0 +1,79 @@
+// Salidas
+
+console.log('Hola, consola!');
+console.log('Hola, :D');
+console.warn('Hola, :D');
+console.error('Hola, :D');
+
+// alert('soy una alerta');
+
+// Entrada
+//prompt('Ingresa tu nombre');
+
+// variables
+// Tipos de dato -> strings, number, boolean, null, undefined
+var nombre = 'Luisa';
+
+console.log('Luisa');
+console.log(nombre);
+
+var edadLupita = 20;
+console.log(20)
+console.log(edadLupita);
+
+var edadJorge = 40;
+console.log(40)
+console.log(edadJorge);
+
+// concatenar -> unir más de una cosa en una variable o como salida
+
+console.log('me llamo ' + nombre);
+console.log(`me llamo ${nombre}`);
+
+// Operadores matemáticos
+// + -> sumas o concatenar
+
+console.log(10 + 30);
+console.log('10' + '30');
+console.log(10 + '10');
+
+// Coerción LEER explícita e implícita
+
+// - -> restas
+console.log(10-50);
+// / -> divisiones
+console.log(10/2);
+// * -> multiplicaciones
+console.log(10*2);
+// % -> módulo -> el residuo de una división
+console.log(10%2) // el residuo de dividir 10/2
+
+// Operadores lógicos -> de comparación
+
+console.log(5 > 3);
+console.log(5 < 3);
+console.log(5 >= 3)
+console.log(5 >= 10)
+console.log(5 >= 5)
+
+// = -> asignación
+// == -> comparación de igualdad NO estricta -> sólo compara el valor y NO compara el tipo de dato
+// === -> comparación de igualdad ESTRICTA -> compara tanto el valor como el tipo de dato
+
+console.log(10 == 8);
+console.log(10 == 10);
+console.log(10 == '10'); 
+console.log(10 === '10');
+
+console.log(10 != 1);
+console.log(10 != 10);
+console.log(10 != '10');
+console.log(10 !== '10');
+
+
+
+
+
+
+
+
