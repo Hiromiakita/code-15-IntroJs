f325d79ef73b899fe1654002f498f040be72d2ca
hiromiakitao@gmail.com <hiromiakitao@gmail.com>
2022-03-15 19:08:23 -0600

    intro funciones

---
+++ 6_funciones/app.js
@@ -0,0 +1,40 @@
+// FUNCIONES
+nombreFuncion();
+
+{
+    // SCOPE
+}
+console.log('mensaje fuera de una función');
+console.log('mensaje fuera de una función');
+console.log('mensaje fuera de una función');
+console.log('mensaje fuera de una función');
+console.log('mensaje fuera de una función');
+
+// Estructura y creación de una función
+function nombreFuncion () {
+    console.log('FUNCIÓN');
+    console.log('sigo siendo la función');
+    // Instrucciones
+    // Las instrucciones declaradas dentro del cuerpo de una fucnió
+    // sólo se ejecutarán cuando yo invoque a la función
+}
+
+// Mandar llamar-Ejecutar-Invocar-Correr una función
+// Con su nombre y paréntesis
+nombreFuncion();
+nombreFuncion();
+nombreFuncion();
+nombreFuncion();
+nombreFuncion();
+
+function saludar() {
+    // Instrucciones
+    let nombre = 'Hiromi';
+    console.log(`Hola ${nombre}`);
+
+}
+
+saludar();
+saludar();
+saludar();
+saludar();
---
+++ 6_funciones/ejercicios1.js
@@ -0,0 +1,5 @@
+// 1. Crear una función que nos muestre en consola los días de la semana.
+
+// 2. Crear una función que nos muestre un numero A, un numero B y la suma de A y B
+
+// 3. Crear una función que nos muestre la longitud de un arreglo que tú crearás
