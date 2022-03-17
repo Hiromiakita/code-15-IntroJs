c7edb585cf3c4aa4a5b3f1b9a6dca7d08d7d8190
hiromiakitao@gmail.com <hiromiakitao@gmail.com>
2022-03-16 19:57:59 -0600

    challenge cajero

---
+++ 6_funciones/challengs.js
@@ -0,0 +1,4 @@
+// Con ayuda de alerts y promts haremos un cajero automático
+// Nos permitirá abrir una cuenta con mínimo $100
+// De no ingresar el monto mínimo, no se podrá crear
+// Una vez creado, se podrá ingresar o retirar dinero
