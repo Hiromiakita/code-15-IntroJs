e0e8a5cf4fafcf6fa102cd4181b38e26ffb83da1
hiromiakitao@gmail.com <hiromiakitao@gmail.com>
2022-03-16 19:16:45 -0600

    ejercicios

--- 6_funciones/ejercicios3.js
+++ 6_funciones/ejercicios3.js
@@ -1,3 +1,7 @@
 // 1. Crear una función que reciba un array de strings y números como este [4,"dos",8,"tres",5,9,1,"cero" y nos regrese un arreglo con sólo los elementos tipo number
 
 // 2. Crear una función que reciba un número como parámetro nos devuelva un arreglo con los números pares desde el 0 hasta el número recibido como parámetro
+
+// 3. Crear una función que me entregue un número entero random entre 1 y 100
+
+// 4. Crear una función que me ayude a codificar un mensaje, la función debe recibir un string y entregarme el mensaje codificado, la codificación reemplaza las 'a' con 4, las 'e' con 3, las 'i' con 7, las 'o' con 6 y las 'u' con 2
