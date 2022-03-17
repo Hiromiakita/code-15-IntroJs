71fd355203d80f42705557acb33429e3a4b90f87
hiromiakitao@gmail.com <hiromiakitao@gmail.com>
2022-03-16 19:08:43 -0600

    ejercicios return

---
+++ 6_funciones/ejercicios3.js
@@ -0,0 +1,3 @@
+// 1. Crear una función que reciba un array de strings y números como este [4,"dos",8,"tres",5,9,1,"cero" y nos regrese un arreglo con sólo los elementos tipo number
+
+// 2. Crear una función que reciba un número como parámetro nos devuelva un arreglo con los números pares desde el 0 hasta el número recibido como parámetro
---
+++ 6_funciones/ejercicios4.js
@@ -0,0 +1,33 @@
+
+// Ejercicio Functions
+
+// Haz un Semáforo.
+
+// El semáforo deberá cambiar de luz de Verde a Amarilla y de Amarilla a Roja y luego de nuevo a Verde.
+
+// Puedes mostrar la imagen del semáforo cambiando, hacer el cambio con console.log o con alert.
+
+// Se debe respetar el orden de los colores:
+
+// De rojo pasa a Verde.
+// De amarillo puede pasar a Rojo.
+// De Verde pasa a Amarillo.
+
+// Extra Challenge 1: Hacerlo con imágenes:
+	
+// 	document.write(“<img src=’http://imagenes.com/luz_verde.png’ >”)
+
+// Extra Challenge 2: Usar setInterval y/o setTimeOut para que cambie automáticamente
+
+// function imprimeHola() {
+// 	console.log("Hola")
+// }
+
+// function detente(intervalo) {
+// 	clearInterval(intervalo)
+// 	console.log("Listo!")
+// }
+
+// var intervalo = setInterval(imprimeHola(),1000)
+
+// setTimeout(detener(intervalo),5000)
